4af74f5e353d1d98411884b24050f82edfea894f
Harris <hglane@crimson.ua.edu>
2020-02-01 18:59:12 -0600

    Updates

---
+++ precache-manifest.233f009a867875da51ce367402f3de9e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8f1f01b0b4707c982647254adf4b120",
+    "url": "//github.com/hglane/clickygame/index.html"
+  },
+  {
+    "revision": "86203e8cd6c65aaa4b7b",
+    "url": "//github.com/hglane/clickygame/static/css/main.07f8b1f4.chunk.css"
+  },
+  {
+    "revision": "831dd2705ca8809181c4",
+    "url": "//github.com/hglane/clickygame/static/js/2.2a2324dd.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "//github.com/hglane/clickygame/static/js/2.2a2324dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86203e8cd6c65aaa4b7b",
+    "url": "//github.com/hglane/clickygame/static/js/main.cbaf3e36.chunk.js"
+  },
+  {
+    "revision": "9e19cd06e6054a7426d6",
+    "url": "//github.com/hglane/clickygame/static/js/runtime-main.06585dc8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "//github.com/hglane/clickygame/static/media/logo.5d5d9eef.svg"
+  }
+]);
