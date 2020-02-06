88673cfaca921af88ab520b77a978ab0d46f71d5
Harris <hglane@crimson.ua.edu>
2020-02-05 18:03:32 -0600

    Updates

---
+++ precache-manifest.3b3fb560621a0334f9f724d31ff27c06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c6ad4f4136b7521e014577663577c23",
+    "url": "/hglane/clickygame/index.html"
+  },
+  {
+    "revision": "86038adae3e4271c8cc5",
+    "url": "/hglane/clickygame/static/css/main.ad82b1ae.chunk.css"
+  },
+  {
+    "revision": "831dd2705ca8809181c4",
+    "url": "/hglane/clickygame/static/js/2.2a2324dd.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/hglane/clickygame/static/js/2.2a2324dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86038adae3e4271c8cc5",
+    "url": "/hglane/clickygame/static/js/main.51ecffcf.chunk.js"
+  },
+  {
+    "revision": "00bb30bc2034aa218ade",
+    "url": "/hglane/clickygame/static/js/runtime-main.6e67971b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/hglane/clickygame/static/media/logo.5d5d9eef.svg"
+  }
+]);
